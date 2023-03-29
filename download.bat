c03e42cf53ebe6da5ffce98b1597b8697cbaeda8
Eligijus Stugys <eligijus.stugys@real-digital.de>
2023-03-29 08:49:57 +0300

    Added new data

--- download.bat
+++ download.bat
@@ -2,4 +2,8 @@
 
 php download.php
 
+git add *
+git commit -a -m "Added new data"
+git push origin main
+
 pause
--- download.php
+++ download.php
@@ -4,8 +4,11 @@
 //error_reporting(E_ALL & ~E_NOTICE);
 
 $html = file_get_contents('https://www.siauliai.lt/lt/list/view/posedziu-klausimai2');
+
 $html = str_replace('href="/', 'href="https://www.siauliai.lt/', $html);
 $html = str_replace('content="/', 'content="https://www.siauliai.lt/', $html);
+$html = str_replace('src="/', 'src="https://www.siauliai.lt/', $html);
+
 $date = date_format(new DateTime(), 'Y-m-d');
 $fileName = "$date.html";
 $dataIndex = [];
